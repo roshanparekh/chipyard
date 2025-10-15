312b8eb392954d3bc75942601ebfcb2bf36afb99
Roshan Parekh <roshanparekh@berkeley.edu>
2025-10-14 21:30:04 -0700

    updated ucie-basic addresses

--- tests/ucie-basic.c
+++ tests/ucie-basic.c
@@ -14,9 +14,9 @@
 
 
 #define UCIE_OFFSET 0x100000000L
-#define MBUS_SPAD 0x08000000L //64 KiB to 0x0801_0000
+#define SPAD_OFFSET 0x580000000L
+#define UCIE_OFFCHIP_BASE (UCIE_OFFSET + SPAD_OFFSET)
 #define CHIP_ID 0x2000L
-#define DRAM_OFFSET 0x80050000L // to 0x8fff_ffff
 
 volatile uint32_t src[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 volatile uint32_t dest[10];
@@ -26,37 +26,24 @@ int main(void) {
 
     uint64_t chipID = reg_read64(CHIP_ID);
 
-    volatile int count = 0;
-
+    // Chip 0 do some stuff
     if (chipID == 0) {
-        printf("Triggering MB Train\n");
-        reg_write64(UCIEPHYTEST_TRIGGERNEW, 1);
-        printf("Trigged\n");
-
-        while(count != 100000) {
-            count++;
-            if ((count % 1000) == 0) {
-                printf("Wait count: %d", count);
-            }
-        }
-
         int val = 10;
-        printf("Writing %d to %p\n", val, (void *) UCIE_OFFSET);
-        reg_write64(UCIE_OFFSET + MBUS_SPAD, val);
+        printf("Writing %d to %p\n", val, (void *) UCIE_OFFCHIP_BASE);
+        reg_write64(UCIE_OFFCHIP_BASE, val);
         printf("Done writing\n");
 
-        printf("Value after read: %ld\n", reg_read64(UCIE_OFFSET + MBUS_SPAD));
+        printf("Value after read: %ld\n", reg_read64(UCIE_OFFCHIP_BASE));
     }
 
-    // if (chipID == 1) {
-    //     printf("Triggering MB Train\n");
-    //     reg_write64(UCIEPHYTEST_TRIGGERNEW, 1);
-    //     printf("Trigged\n");
-    // }
+    // Chip 1 do some stuff
+    if (chipID == 1) {
+        
+    }
 
     volatile int k = 0;
     while(1){
-        if ((k % 10) == 0) {
+        if ((k % 10000) == 0) {
             printf("[ChipID: %ld] Busy waiting...\n", chipID);
         }
         k++;
